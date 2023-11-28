3544f002683b3bc0db0852d4dddee28ef908304b
Faizan Khan <37953355+faizi20@users.noreply.github.com>
2023-11-29 00:00:03 +0500

    Create task2.sh
    github file upload auto

---
+++ task2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo FROM python:3
+echo RUN pip3 install django
+echo COPY . .
+echo RUN python manage.py migrate
+
+echo CMD ["python", "manage.py", "runserver", "0.0.0.0:8001"]
+
+sudo docker run -p 8001:8001 a0bd6a58a96c
